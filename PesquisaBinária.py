eb80ede151eb08cb9f1275b3c5ed58b4ed5d0597
João Henrique <126480950+Joaohnt27@users.noreply.github.com>
2023-05-18 20:59:20 -0300

    Update PesquisaBinária.py

--- PesquisaBinária.py
+++ PesquisaBinária.py
@@ -2,7 +2,7 @@
 
 def pesquisa_binaria(lista, item):
     baixo = 0
-    alto = len(lista) - 1  # Verificando o tamanho/comprimento (lenght) da lista, ou seja o número de elementos contidos nela, que possui 5 elementos mas precisa de subtrair 1 para ficar com 4
+    alto = len(lista) - 1  # Verificando o tamanho/comprimento (lenght) da lista, ou seja o número de elementos contidos nela
 
     while baixo <= alto:
         meio = (baixo + alto) // 2
